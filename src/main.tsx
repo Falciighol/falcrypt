1ef2071346c130d1d0848b8ab6a1125fe4254817
Juan Hernandez <juampa.hernandez@outlook.com>
2025-06-22 16:57:08 -0600

    chore: remove unnecessary semicolons and improve linting script

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
 import { StrictMode } from 'react'
-import { ConfigProvider as ThemeProvider, theme } from 'antd';
+import { ConfigProvider as ThemeProvider, theme } from 'antd'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 // get url params
-const urlParams = new URLSearchParams(window.location.search);
-const themeParam = urlParams.get('theme');
+const urlParams = new URLSearchParams(window.location.search)
+const themeParam = urlParams.get('theme')
 
-const selectedTheme = themeParam === 'light' ? 'defaultAlgorithm' : 'darkAlgorithm';
+const selectedTheme = themeParam === 'light' ? 'defaultAlgorithm' : 'darkAlgorithm'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider
       theme={{
       algorithm: theme[selectedTheme],
-      "token": {
+      'token': {
         fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto',
       },
     }}>
